8920f347a321fec88504b3cedebe226325a6323d
Valeriya Burkova <valeriya.burkova@student.gymspit.cz>
2021-11-15 21:04:00 +0100

    hotový program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace Covid
 {
     class CovidProgram
     {
-        static int[] cases = new int[] { 0, 4, 15, 30, 45, 60, 82, 110, 150, 193, 200, 205, 199, 170, 150, 153, 160, 200 };
+        static int[] cases = new int[] { 0, 4, 15, 30, 45, 60, 82, 110, 150, 193, 200, 205, 199, 170, 150, 153, 160, 200, 235, 250, 260};
 
         static void Main(string[] args)
         {
@@ -18,20 +18,15 @@ namespace Covid
                         Console.WriteLine();
                         for (int i = 0; i < cases.Length; i = i + 1)
                         {
-                            // Den 1, p59pad 10, R=0
-                            Console.WriteLine("Den {0} přibylo {1} případů, R = {2}", i + 1, cases[i], simpleR(i));
-                            // Console.WriteLine("Den " + (i + 1) + " přibylo " + cases[i] + " případů, R = " + simpleR(i));
+                            Console.WriteLine("Den {0} přibylo {1} případů, R = {2}, incidence7 = {3}, incidence14 = {4} ", 
+                                i + 1, cases[i], simpleR(i), incidence7(i), incidence14(i));
                         }
                         Console.WriteLine();
                         Console.WriteLine("Máme záznam z " + cases.Length + " dnů");
                         if (cases.Length > 0)
                         {
-                            Console.WriteLine("Pokud R=0, chybí pro výpočet R údaje.");
-                            Console.WriteLine("Průměr je " + casesPerDayAverage());
-                            Console.WriteLine("Počet nakažených na 100 tisíc obyvatel je " + casesPer100K());
-                            Console.WriteLine((minDay(out int minimum) + 1) + ".den bylo nalezeno minimum: " + minimum);
-                            int dayx = maxDay();
-                            Console.WriteLine(dayx + ".den" + " bylo nalezeno maximum: " + cases[dayx]);
+                            Console.WriteLine("Pokud je R nebo incidence 0, chybí pro výpočet údaje.");
+                            printStats(0, cases.Length-1);
                         }
                         break;
                     case 2:
@@ -39,7 +34,7 @@ namespace Covid
                         int day = readPositiveInteger();
                         if (day > cases.Length - 1)
                         {
-                            Console.WriteLine("Chyné zadání, vyberte prosím existující záznam.");
+                            Console.WriteLine("Chybné zadání, vyberte prosím existující záznam.");
                         }
                         else
                         { 
@@ -59,7 +54,7 @@ namespace Covid
                         Console.WriteLine("Záznamy byly smazány.");
                         break;
                     case 5:
-                        Console.WriteLine("Predikce vývoje");
+                        Console.WriteLine("Predikci vývoje jsem nestihla");
                         break;
                     case 6:
                         printDaysRange();
@@ -78,18 +73,13 @@ namespace Covid
             Console.WriteLine("1: Zobrazit záznam případů");
             Console.WriteLine("2: Upravit záznam");
             Console.WriteLine("3: Přidat záznam");
-            Console.WriteLine("4: Smazat záznam");
+            Console.WriteLine("4: Smazat záznamy");
             Console.WriteLine("5: Zobrazit predikci vývoje");
             Console.WriteLine("6: Zobrazit záznam v rozsahu dnů");
             Console.WriteLine("7: Ukončit program");
             Console.WriteLine();
         }
 
-        /*
-        * Přečte hodnotu zadanou uživatelem. 
-        * Vrátí TRUE, zadal-li uživatel platné celé číslo. 
-        * Vrátí FALSE, pokud zadanou hodnotu nelze na celé číslo převést.
-        */
         static bool tryReadInteger(out int result)
         {
             string s = Console.ReadLine();
@@ -100,7 +90,7 @@ namespace Covid
         {
             int result;
             printMenu();
-            while (!tryReadInteger(out result) || result < 1 || result > 6)
+            while (!tryReadInteger(out result) || result < 1 || result > 7)
             {
                 Console.WriteLine();
                 Console.WriteLine("Chybné zadání, napište platné číslo operace: ");
@@ -120,29 +110,50 @@ namespace Covid
             }
             return result;
         }
-        static double casesPerDayAverage()
+        
+        static double incidence7(int day)
         {
-            double soucet = 0;
-            for (int i = 0; i < cases.Length; i = i + 1)
+            if (day < 6)
             {
-                soucet = soucet + cases[i];
+                return 0;
             }
-            return Math.Round(soucet / cases.Length, 2);
+            double count = 0;
+            for (int i= day-6; i <= day ; i++)
+            {
+                count = count + cases[i];
+            }
+            return Math.Round(count/ (10000000 / 100000), 2);
         }
-        static double casesPer100K()
+
+        static double incidence14(int day)
         {
+            if (day < 13)
+            {
+                return 0;
+            }
+            double count = 0;
+            for (int i = day - 13; i <= day; i++)
+            {
+                count = count + cases[i];
+            }
+            return Math.Round(count / (10000000 / 100000), 2);
+        }
+
+        static double casesPer100K(int firstDay, int lastDay)
+        {
+            int dayCount = lastDay - firstDay + 1;
             double soucet = 0;
-            for (int i = 0; i < cases.Length; i = i + 1)
+            for (int i = firstDay; i <= lastDay; i++)
             {
                 soucet = soucet + cases[i];
             }
             return soucet * 100000 / 10000000;
         }
-        static int minDay(out int minimum)
+        static int minDay(int firstDay, int lastDay, out int minimum)
         {
             int day = -1;
             minimum = int.MaxValue;
-            for (int j = 1; j < cases.Length; j++)
+            for (int j = firstDay; j <= lastDay; j++)
             {
                 if (cases[j] < minimum)
                 {
@@ -152,11 +163,11 @@ namespace Covid
             }
             return day;
         }
-        static int maxDay()
+        static int maxDay(int firstDay, int lastDay)
         {
             int day = -1;
             int maximum = int.MinValue;
-            for (int i = 0; i < cases.Length; i++)
+            for (int i = firstDay; i <= lastDay; i++)
             {
                 if (cases[i] > maximum)
                 {
@@ -168,8 +179,6 @@ namespace Covid
         }
         static double simpleR(int day)
         {
-            // R(i) = ( N(i) + N(i-1) + N(i-2) + N(i-3) + N(i-4) + N(i-5) + N(i-6) ) / ( N(i-5) + N(i-6) + N(i-7) + N(i-8) + N(i-9) + N(i-10) + N(i-11) )
-            //cases[day] +
            if (day < 11)
             {
                 return 0;
@@ -191,20 +200,37 @@ namespace Covid
             int firstDay = readDayNumber();
             Console.WriteLine("Napište koncový den: ");
             int lastDay = readDayNumber();
-            for (int i = firstDay; i<=lastDay; i++)
+            for (int i = firstDay; i <= lastDay; i++)
             {
-                Console.WriteLine("Den {0} přibylo {1} případů, R = {2}", i + 1, cases[i], simpleR(i));
+                Console.WriteLine("Den {0} přibylo {1} případů, R = {2}, incidence7 = {3}, incidence14 = {4} ",
+                               i + 1, cases[i], simpleR(i), incidence7(i), incidence14(i));
+            }
+            Console.WriteLine("Zobrazuji záznam z {0} až {1} dne.", firstDay+1, lastDay +1);
+            if (lastDay - firstDay + 1 > 0)
+            {
+                Console.WriteLine("Pokud je R nebo incidence 0, chybí pro výpočet údaje.");
+                printStats(firstDay, lastDay);
             }
             return;
 
         }
+        static void printStats(int firstDay, int lastDay)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Průměr pro dny {0} až {1} je: {2}", firstDay + 1, lastDay + 1, casesPerDayAvarage(firstDay, lastDay));
+            Console.WriteLine("Počet nakažených na 100 tisíc obyvatel je " + casesPer100K(firstDay, lastDay));
+            int a = firstDay;
+            if (firstDay == 0)
+            {
+                a = firstDay + 1;
+            }
+            Console.WriteLine((minDay(a, lastDay, out int minimum) + 1) + ".den bylo nalezeno minimum: " + minimum);
+            int dayx = maxDay(firstDay, lastDay);
+            Console.WriteLine(dayx + 1 + ".den" + " bylo nalezeno maximum: " + cases[dayx]);
+        }
+
         static int readDayNumber()
         {
-            // ziskej cislo od uzivatele
-            // kontrola uspesneho zadani
-            // kontrola rozsahu
-            // chyba -> opakuj zadani
-            // v poradku -> vrat den
             int result;
             while (!tryReadInteger(out result) || result <1 || result > cases.Length)
             {
@@ -213,18 +239,17 @@ namespace Covid
             }
             return result-1;
         }
-            
-            
-        
-         
+        static double casesPerDayAvarage(int firstDay, int lastDay)
+        {
+            int dayCount = lastDay - firstDay + 1;
+            double caseCount = 0;
+            for (int i = firstDay; i <= lastDay; i++)
+            {
+                caseCount = caseCount + cases[i];
+            }
+            return Math.Round (caseCount/dayCount, 2);
+        }
     }
-
-
-
-
-
-
-
 } 
     
 
